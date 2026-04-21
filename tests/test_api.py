02f4bd1512d4c80bd2e141bfa4ea6b9223b92afd
spiridonov-dev <spiridonovv264@gmail.com>
2026-04-21 20:48:56 +0400

    Create test_api.py

---
+++ tests/test_api.py
@@ -0,0 +1,13 @@
+import requests
+
+def test_get_post():
+    url = "https://jsonplaceholder.typicode.com/posts/1"
+
+    response = requests.get(url)
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert "userId" in data
+    assert "title" in data
